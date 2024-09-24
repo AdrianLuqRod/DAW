1a5d3710d02ce983f3332e4ed1201fd59e27bf1d
Adri <adrian.luque.rod@gmail.com>
2024-09-24 09:49:14 +0200

    b04_ej01ej04

--- 2ºDAW/DWES/RepasoJava/src/main/java/boletin04/ej02.java
+++ 2ºDAW/DWES/RepasoJava/src/main/java/boletin04/ej02.java
@@ -4,8 +4,20 @@ package boletin04;
 TODO: Leer 5 números y mostrarlos en orden inverso al introducido.
  */
 
+import java.util.Scanner;
+
 public class ej02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[5];
+        System.out.println("Introduce 5 números:");
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            numeros[i] = sc.nextInt();
+        }
 
+        System.out.println("Números en orden inverso:");
+        for (int numero : numeros) {
+            System.out.printf(numero + " ");
+        }
     }
 }
--- 2ºDAW/DWES/RepasoJava/src/main/java/boletin04/ej03.java
+++ 2ºDAW/DWES/RepasoJava/src/main/java/boletin04/ej03.java
@@ -5,8 +5,29 @@ TODO: Leer 5 números por teclado y a continuación realizar la media de los nú
  y contar el número de ceros.
  */
 
+import java.util.Scanner;
+
 public class ej03 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[5];
+        int mediaCeros = 0, mediaPositivos = 0, mediaNegativos = 0, ceros = 0, positivos = 0, negativos = 0;
+        System.out.println("Introduce 5 números: ");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = sc.nextInt();
+            if (numeros[i] == 0) {
+                mediaCeros += numeros[i];
+                ceros++;
+            } else if (numeros[i] > 0) {
+                mediaPositivos += numeros[i];
+                positivos++;
+            } else {
+                mediaNegativos += numeros[i];
+                negativos++;
+            }
+        }
+        mediaCeros /= ceros;
+        mediaPositivos /= positivos;
+        mediaNegativos /= negativos;
     }
 }
