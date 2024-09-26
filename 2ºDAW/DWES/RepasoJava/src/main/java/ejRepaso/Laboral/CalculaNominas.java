30bd5a77a5200689c1dab47c6057a282abd70dda
Adri <adrian.luque.rod@gmail.com>
2024-09-26 18:39:33 +0200

    repasoEjercicio

--- 2ºDAW/DWES/RepasoJava/src/main/java/ejRepaso/Laboral/CalculaNominas.java
+++ 2ºDAW/DWES/RepasoJava/src/main/java/ejRepaso/Laboral/CalculaNominas.java
@@ -1,25 +1,48 @@
 package ejRepaso.Laboral;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculaNominas {
+    private static final String RUTA = System.getProperty("user.dir") + "/RepasoJava/src/main/java/ejRepaso/Laboral/";
 
     public static void main(String[] args) {
+        List<Empleado> empleados = leerEmpleados(RUTA + "empleados.txt");
         try {
-            Empleado e1 = new Empleado('M', "32000032G", "James Cosling", 4, 7);
-            Empleado e2 = new Empleado('F', "23200032R", "Ada Lovelace");
-            escribe(e1, e2);
-            e1.incrAnyo();
-            e2.setCategoria(9);
-            escribe(e1, e2);
-        } catch (DatosNoCorrectosException e) {
-            System.out.println(e.getMessage());
+            escribirSueldos(RUTA + "sueldos.dat", empleados);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void escribe(Empleado e1, Empleado e2) {
+    private static List<Empleado> leerEmpleados(String archivo) {
+        List<Empleado> empleados = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(",");
+                char sexo = datos[0].charAt(0);
+                String dni = datos[1];
+                String nombre = datos[2];
+                int categoria = Integer.parseInt(datos[3]);
+                int anyos = Integer.parseInt(datos[4]);
+                empleados.add(new Empleado(sexo, dni, nombre, categoria, anyos));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return empleados;
+    }
+
+    private static void escribirSueldos(String archivo, List<Empleado> empleados) throws IOException {
         Nomina n = new Nomina();
-        e1.imprime();
-        e2.imprime();
-        System.out.println(n.sueldo(e1)+"€");
-        System.out.println(n.sueldo(e2)+"€");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+        for (Empleado e : empleados) {
+            bw.write(e.dni + ", " + n.sueldo(e) + "€");
+            bw.newLine();
+            bw.flush();
+        }
+        bw.close();
     }
 }
