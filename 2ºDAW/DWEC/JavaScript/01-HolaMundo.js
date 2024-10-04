9d548bb1162d57a767fa479706737dabe3978435
Adri <adrian.luque.rod@gmail.com>
2024-10-04 18:17:01 +0200

    js

---
+++ 2ºDAW/DWEC/JavaScript/01-HolaMundo.js
@@ -0,0 +1,21 @@
+"use strict";
+if (true) {
+  var mensaje = "Hola MundoVar";
+  let mensajeLet = "Hola MundoLet";
+}
+console.log(mensaje);
+// console.log(mensajeLet); <-- Da error no existe
+
+if (false) {
+  var mensaje2 = "Hola MundoVar2";
+}
+console.log(mensaje2); // <-- No da error, devuelve undefined.
+
+const variable3 = 2;
+
+//   yo tambien madlita sea :(
+// - yo tambien madlita sea :(
+// . yo tambien madlita sea :(
+// ? yo tambien madlita sea :(
+// ! yo tambien madlita sea :(
+//// yo tambien madlita sea :(
