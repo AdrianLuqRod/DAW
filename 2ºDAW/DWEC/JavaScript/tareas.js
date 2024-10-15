12430e873fe84f125cd097c1d0e134872c42f55e
Adrian <adrian.luque.rod@gmail.com>
2024-10-15 20:49:12 +0200

    js

--- 2ºDAW/DWEC/JavaScript/tareas.js
+++ 2ºDAW/DWEC/JavaScript/tareas.js
@@ -1,24 +1,24 @@
-for (let index = 100; index < 1000; index++) {
-  let centenas = parseInt(index / 100);
-  let decenas = parseInt((index % 100) / 10);
-  let unidades = parseInt(index % 10);
-  centenas = centenas ** 3;
-  decenas = decenas ** 3;
-  unidades = unidades ** 3;
-  let numero = Number(centenas) + Number(decenas) + Number(unidades);
-  if (Number(numero) === index) {
-    console.log(index);
-  }
+function ask2(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
 }
 
-for (let index = 100; index < 1000; index++) {
-  let centenas = Math.floor(index / 100);
-  let decenas = Math.floor((index % 100) / 10);
-  let unidades = index % 10;
-  if (
-    index ===
-    Math.pow(centenas, 3) + Math.pow(decenas, 3) + Math.pow(unidades, 3)
-  ) {
-    console.log(index);
-  }
+ask(
+  "Do you agree?",
+  function () { alert("You agreed."); },
+  function () { alert("You canceled the execution."); }
+)
+
+
+
+let ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+
 }
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+)
