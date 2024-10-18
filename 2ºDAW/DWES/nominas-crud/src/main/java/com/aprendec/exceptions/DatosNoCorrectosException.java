9bbe46854a7a158032ffb4561eb41b3eac8e00ea
Adri <adrian.luque.rod@gmail.com>
2024-10-18 12:52:15 +0200

    ds

--- 2ºDAW/DWES/nominas-crud/src/main/java/com/aprendec/exceptions/DatosNoCorrectosException.java
+++
@@ -1,7 +0,0 @@
-package com.aprendec.exceptions;
-
-public class DatosNoCorrectosException extends RuntimeException {
-    public DatosNoCorrectosException(String message) {
-        super(message);
-    }
-}
