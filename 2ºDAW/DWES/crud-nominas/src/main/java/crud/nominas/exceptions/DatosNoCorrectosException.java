a4e72c0c4802e4b94a3b2c2f6c2c2f56793c58ac
Adrian <adrian.luque.rod@gmail.com>
2024-10-19 18:01:25 +0200

    delete-crud

--- 2ºDAW/DWES/crud-nominas/src/main/java/crud/nominas/exceptions/DatosNoCorrectosException.java
+++
@@ -1,7 +0,0 @@
-package crud.nominas.exceptions;
-
-public class DatosNoCorrectosException extends RuntimeException {
-    public DatosNoCorrectosException(String message) {
-        super(message);
-    }
-}
